1d36286544a453bb5908b020cf38845eea1f3d59
busidoway <busidowayfarer@gmail.com>
2024-02-17 17:05:41 +0500

    Create smsc functions and constants

---
+++ config/smsc.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'SMSC_LOGIN' => 'login',
+    'SMSC_PASSWORD' => 'password',
+    'SMSC_POST' => 0,
+    'SMSC_HTTPS' => 0,
+    'SMSC_CHARSET' => 'windows-1251',
+    'SMSC_DEBUG' => 0,
+    'SMTP_FROM' => 'api@smsc.ru'
+];
