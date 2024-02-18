26293353ee36b27e505c5e2cbcb2847c33bf87b6
busidoway <busidowayfarer@gmail.com>
2024-02-18 21:25:49 +0500

    Changes in migrations

--- database/migrations/2024_02_16_120848_create_sendsms_table.php
+++ database/migrations/2024_02_16_120848_create_sendsms_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('sendsms', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('message');
             $table->tinyInteger('status')->default('1');
+            $table->dateTime('time');
             $table->timestamps();
         });
     }
